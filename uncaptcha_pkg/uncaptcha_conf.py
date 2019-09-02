60f4351e955f23946a4ea182fd1ad3e73a1a2db9
Min-Haw, Liu <liuminhaw@gmail.com>
2019-09-03 05:13:41 +0800

    update: coordinates data return value
    Origin: Return string value - '(x-pos, y-pos)'
    After: Return tuple - (x-pos, y-pos)

--- uncaptcha_pkg/uncaptcha_conf.py
+++ uncaptcha_pkg/uncaptcha_conf.py
@@ -60,62 +60,82 @@ class Config():
     def private_browser_coords(self):
         """
         Return config PRIVATE_BROWSER_COORDS option in CURSOR section
+        Return:
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.PRIVATE_BROWSER_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.PRIVATE_BROWSER_COORDS))
 
     def search_coords(self):
         """
         Return config SEARCH_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.SEARCH_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.SEARCH_COORDS))
 
     def google_coords(self):
         """
         Return config GOOGLE_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.GOOGLE_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.GOOGLE_COORDS))
 
     def captcha_coords(self):
         """
         Return config CAPTCHA_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.CAPTCHA_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.CAPTCHA_COORDS))
 
     def check_coords(self):
         """
         Return config CHECK_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.CHECK_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.CHECK_COORDS))
 
     def audio_coords(self):
         """
         Return config AUDIO_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.AUDIO_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.AUDIO_COORDS))
 
     def download_coords(self):
         """
         Return config DOWNLOAD_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.DOWNLOAD_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.DOWNLOAD_COORDS))
 
     def final_coords(self):
         """
         Return config FINAL_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.FINAL_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.FINAL_COORDS))
 
     def verify_coords(self):
         """
         Return config VERIFY_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.VERIFY_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.VERIFY_COORDS))
 
     def close_coords(self):
         """
         Return config CLOSE_COORDS option in CURSOR section
+        Return: 
+            xy coordinate tuple
         """
-        return self._read_value(self.CURSOR_SEC, self.CLOSE_COORDS)
+        return self._transfer_coords(self._read_value(self.CURSOR_SEC, self.CLOSE_COORDS))
 
     def private_color(self):
         """
@@ -140,25 +160,25 @@ class Config():
         """
         Test to make sure there is value for all options
         """
-        _re_coord_pattern = re.compile(r'^\([0-9]*,\s*[0-9]*\)')
+        _re_coord_pattern = re.compile(r'^[0-9]*,\s*[0-9]*')
         _re_color_pattern = re.compile(r'#[0-9a-fA-F]{6}')
 
         _download_location = self.download_location()
         _search_coords = self.search_coords()
 
         if not os.path.isdir(self.download_location()):
-            raise OptionFormatError('{} not exist'. format(self.DOWNLOAD_LOCATION))
+            raise OptionFormatError(self.DOWNLOAD_LOCATION, self._read_value(self.FILE_SEC, self.DOWNLOAD_LOCATION))
             
-        self._regex_test(_re_coord_pattern, self.private_browser_coords(), self.PRIVATE_BROWSER_COORDS)
-        self._regex_test(_re_coord_pattern, self.search_coords(), self.SEARCH_COORDS)
-        self._regex_test(_re_coord_pattern, self.google_coords(), self.GOOGLE_COORDS)
-        self._regex_test(_re_coord_pattern, self.captcha_coords(), self.CAPTCHA_COORDS)
-        self._regex_test(_re_coord_pattern, self.check_coords(), self.CHECK_COORDS)
-        self._regex_test(_re_coord_pattern, self.audio_coords(), self.AUDIO_COORDS)
-        self._regex_test(_re_coord_pattern, self.download_coords(), self.DOWNLOAD_COORDS)
-        self._regex_test(_re_coord_pattern, self.final_coords(), self.FINAL_COORDS)
-        self._regex_test(_re_coord_pattern, self.verify_coords(), self.VERIFY_COORDS)
-        self._regex_test(_re_coord_pattern, self.close_coords(), self.CLOSE_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.PRIVATE_BROWSER_COORDS), self.PRIVATE_BROWSER_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.SEARCH_COORDS), self.SEARCH_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.GOOGLE_COORDS), self.GOOGLE_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.CAPTCHA_COORDS), self.CAPTCHA_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.CHECK_COORDS), self.CHECK_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.AUDIO_COORDS), self.AUDIO_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.DOWNLOAD_COORDS), self.DOWNLOAD_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.FINAL_COORDS), self.FINAL_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.VERIFY_COORDS), self.VERIFY_COORDS)
+        self._regex_test(_re_coord_pattern, self._read_value(self.CURSOR_SEC, self.CLOSE_COORDS), self.CLOSE_COORDS)
 
         self._regex_test(_re_color_pattern, self.private_color(), self.PRIVATE_COLOR)
         self._regex_test(_re_color_pattern, self.google_color(), self.GOOGLE_COLOR)
@@ -179,12 +199,22 @@ class Config():
         """
         try:
             _config_value = self._config.get(section, key)
-        except configparser.NoSectionError as error:
-            raise NoSectionError(error)
-        except configparser.NoOptionError as error:
-            raise NoOptionError(error)
+        except configparser.NoSectionError:
+            raise NoSectionError(section)
+        except configparser.NoOptionError:
+            raise NoOptionError(key)
         else:
             return _config_value
+
+    def _transfer_coords(self, coords):
+        """
+        Transfer string type coordination to tuple type
+        Input:
+            coords: string - (x_pos, y_pos)
+        """
+        coords_list = coords.split(',')
+        return (int(coords_list[0]), int(coords_list[1]))
+        
 
     def _regex_test(self, pattern , value, key):
         """
@@ -195,7 +225,7 @@ class Config():
             key: string - config option key
         """
         if pattern.fullmatch(value) == None:
-            raise OptionFormatError('{} wrong format: {}'.format(key, value))
+            raise OptionFormatError(key, value)
 
 
 
@@ -216,15 +246,19 @@ class NoSectionError(configError):
     """
     Raised by configparser.NoSectionError
     """
-    pass
+    def __init__(self, section):
+        self.message = '{} section not found'.format(section)
 
 class NoOptionError(configError):
     """
     Raised by configparser.NoOptionError
     """
-    pass
+    def __init__(self, option):
+        self.message = '{} option not found'.format(option)
 
 class OptionFormatError(configError):
     """
     Raised if option is in wrong format
     """
+    def __init__(self, option, value):
+        self.message = '{} wrong format: {}'.format(option, value)
